0e7b953e2a1464f6cf479db5ab35b844f37c47f6
yurak <yurakorolev04@gmail.com>
2023-07-06 03:22:51 +0300

    second commit

---
+++ Je veux/js/je_veux.js
@@ -0,0 +1,10 @@
+const bg = document.getElementById('bg');
+const exit = document.getElementById('exit');
+
+
+bg.onclick = () =>{
+   document.body.classList.toggle('active');
+}
+exit.onclick = () =>{
+   document.body.classList.toggle('active');
+}
